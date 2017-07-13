6727eb59f9a332423519ee62ecc30f105cadcec8
fengwei-iri <fengwei-iri@k4450v.add.bjyt.qihoo.net>
2017-07-13 16:45:37 +0800

    添加新的文件

--- webroot/index.php
+++ webroot/index.php
@@ -5,5 +5,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+define("APPLICATION_PATH",  dirname(dirname(__FILE__)));
+define("DIR_SPLIT", "\\");
+include_once(APPLICATION_PATH.DIR_SPLIT."framework".DIR_SPLIT."glider_sky".DIR_SPLIT."core".DIR_SPLIT."GliderSky.php");
 
-echo "hello worde";
+register_shutdown_function(function() {
+    
+});
+
+$oApp = new GliderSky(APPLICATION_PATH."/config/application.ini");
+
+echo "hello world";
