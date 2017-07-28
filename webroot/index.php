b510c3efa25e7c3dd34262dde7c8695fa3ea0939
fengwei-iri <fengwei-iri@k4450v.add.bjyt.qihoo.net>
2017-07-28 16:59:22 +0800

    拆分基础类

--- webroot/index.php
+++ webroot/index.php
@@ -4,7 +4,10 @@ header('Content-Type: text/html; charset=utf-8');
 
 include_once(APPLICATION_PATH."/config/config.inc.php");
 
+$oModule = new GS_Module();
+
 $oTemplate = new GS_Template();
 $oTemplate->loadTemplate();        
+$oApp->setTemplate($oTemplate);
 $oTemplate->assign("helloworld", "hello world")->display("index.tpl");
 
