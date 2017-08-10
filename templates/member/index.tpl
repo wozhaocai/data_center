4b27b1b33aeb7bcf9efdc5369e9b00562175de99
fengwei-iri <fengwei-iri@k4450v.add.bjyt.qihoo.net>
2017-08-10 19:41:13 +0800

    添加登录

--- templates/member/index.tpl
+++ templates/member/index.tpl
@@ -1,1 +1,4 @@
 你好：
+
+
+<a href="/logout.php">退出</a>
--- webroot/service.php
+++ webroot/service.php
@@ -3,7 +3,7 @@ define("APPLICATION_PATH", dirname(dirname(__FILE__)));
 header('Content-Type: text/html; charset=utf-8');
 
 include_once(APPLICATION_PATH."/config/config.inc.php");
-
+debugVar($_SESSION);
 if(empty($_SESSION["username"])){
     Header("Location:/index.php");
 }
