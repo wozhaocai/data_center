c836000e4d3e2bd60ed3c88874e90deb79efd8fd
fengwei-iri <fengwei-iri@k4450v.add.bjyt.qihoo.net>
2017-08-14 18:10:48 +0800

    添加resource表

--- sql/tables.sql
+++ sql/tables.sql
@@ -57,3 +57,14 @@ CREATE TABLE `group_menu_map` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `uk_gid_mid` (`gid`,`mid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户组菜单映射表';
+
+CREATE TABLE `resource` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '资源id',
+  `type` varchar(50) NOT NULL DEFAULT 'xml' COMMENT '资源类型',
+  `service_id` varchar(100) NOT NULL DEFAULT '' COMMENT '服务id',  
+  `content` text NOT NULL DEFAULT '' COMMENT '类型内容',  
+  `ctime` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '创建时间',
+  `mtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '最后修改时间',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uk_resource` (`service_id`,`type`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='资源';
