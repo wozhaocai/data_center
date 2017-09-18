39d5a9c758f8056e4877b0c652c8a799449223f7
hp <hp@192.168.18.1>
2017-09-19 00:12:22 +0800

    设置运行脚本

--- scripts/stock_china.sh
+++ scripts/stock_china.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-/usr/local/bin/php php_job.php -bdc -tscript -sSpider_china_sh_code_list -aget
-/usr/local/bin/php php_job.php -bdc -tworkflow -schina_sh -asave_price
+/usr/local/bin/php /home/wozhad3j/scripts/job.php -bdc -tscript -sSpider_china_sh_code_list -aget >/dev/null 2>&1 
+/usr/local/bin/php /home/wozhad3j/scripts/job.php -bdc -tworkflow -schina_sh -asave_price >/dev/null 2>&1 
 
-/usr/local/bin/php php_job.php -bdc -tscript -sSpider_china_sz_code_list -aget
-/usr/local/bin/php php_job.php -bdc -tworkflow -schina_sz -asave_price
+/usr/local/bin/php /home/wozhad3j/scripts/job.php -bdc -tscript -sSpider_china_sz_code_list -aget >/dev/null 2>&1 
+/usr/local/bin/php /home/wozhad3j/scripts/job.php -bdc -tworkflow -schina_sz -asave_price >/dev/null 2>&1 
 
-/usr/local/bin/php php_job.php -bdc -tscript -sSpider_china_hk_code_list -aget
-/usr/local/bin/php php_job.php -bdc -tworkflow -schina_hk -asave_price
+/usr/local/bin/php /home/wozhad3j/scripts/job.php -bdc -tscript -sSpider_china_hk_code_list -aget >/dev/null 2>&1 
+/usr/local/bin/php /home/wozhad3j/scripts/job.php -bdc -tworkflow -schina_hk -asave_price >/dev/null 2>&1 
