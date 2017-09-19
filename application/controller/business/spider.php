e162e21bf9e9803f07f3d632fac43c1fc87fd707
hp <hp@192.168.18.1>
2017-09-19 21:21:56 +0800

    添加抓取文章内容

--- application/controller/business/spider.php
+++ application/controller/business/spider.php
@@ -11,8 +11,21 @@ class Business_SpiderController Extends BaseController {
     public function getChinaCodePage() {
         $sSpiderUrl = str_replace("{".$this->_aParams["query"]["spider_field"]."}",$this->_aParams["query"]["spider_init_value"],$this->_aParams["query"]["spider_url"]);
         $sContent = substr(Util_Curl::execute($sSpiderUrl),12,-1);
+        $sContent = str_replace("'","",$sContent);
         preg_match_all("/total\:[0-9,]+/", $sContent,$matchs);
         $total = substr(str_replace("total:","",$matchs[0][0]),0,-1);
         return "1,{$total}";
     }
+    
+    public function getChinaNewsByUrl(){
+        $sSpiderUrl = $this->_aParams["query"][3]; 
+        if(!empty($sSpiderUrl)){
+            $oFile = new Util_File();
+            $aContent = $oFile->findByUrl($sSpiderUrl, "TEXT-INDENT: 2em", "Cnt-Main-Article-QQ", "正文已结束", true);   
+            $sContent = implode("\n",$aContent);
+            return $sContent;
+        }else{
+            return "";
+        }
+    }       
 }
