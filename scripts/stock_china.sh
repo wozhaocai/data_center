8684b6e95468193f4ebf881a2d6c20e2195018b5
hp <hp@192.168.18.1>
2017-09-20 18:28:24 +0800

    添加脚本

--- scripts/stock_china.sh
+++ scripts/stock_china.sh
@@ -7,3 +7,10 @@
 
 /usr/local/bin/php /home/wozhad3j/scripts/job.php -bdc -tscript -sSpider_china_hk_code_list -aget >/dev/null 2>&1 
 /usr/local/bin/php /home/wozhad3j/scripts/job.php -bdc -tworkflow -schina_hk -asave_price >/dev/null 2>&1 
+
+/usr/local/bin/php /home/wozhad3j/scripts/job.php -bdc -tworkflow -schina_a_finance_news -asave_sh_finance_news >/dev/null 2>&1 
+/usr/local/bin/php /home/wozhad3j/scripts/job.php -bdc -tworkflow -schina_a_finance_news -asave_sz_finance_news >/dev/null 2>&1 
+/usr/local/bin/php /home/wozhad3j/scripts/job.php -bdc -tworkflow -schina_hk_finance_news -asave_finance_news >/dev/null 2>&1 
+
+echo `date`
+echo ' stock_china.sh completed'
